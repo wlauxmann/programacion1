ed7305ee160b54f4ebc1a0532fafb766db44723c
wlauxmann <wlauxmann@gmail.com>
2015-04-29 12:25:54 -0300

    Cálculo de la superficie de un triángulo
    Cálculo de la superficie de un triángulo con Python

---
+++ superficie.py
@@ -0,0 +1,9 @@
+# Cálculo de la superficie de un triángulo
+
+sup = 0
+base = 0
+altura = 0
+base = input("Ingrese la base: ")
+altura = input("Ingrese la altura: ")
+sup = base * altura / 2
+print "La Superficie es: ",sup
